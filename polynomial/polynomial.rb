f40220dd4f647dd0c7d549b14d24e10cdc88904b
Olegborodko <220v2@mail.ru>
2014-11-06 12:53:52 +0200

    hm..

---
+++ polynomial/polynomial.rb
@@ -0,0 +1,104 @@
+class Polynomial
+
+	def initialize(a)
+
+		if a.length<3
+			raise ArgumentError.new("Two or more coefficients are required")
+		end
+
+		@my_mas=a.reverse
+	end
+
+	def create_polynomial
+
+		step=0 #шаг и степень
+		my_print=''
+
+
+		@my_mas.each do |el|
+
+			element=el.to_i
+
+			if element==0
+				step+=1
+				next
+			end
+
+			if step==0
+				
+				my_print="+#{element}" 
+				step+=1
+				next
+			end
+
+			if (element==1 and step==1)
+				
+				my_print="+x#{my_print}" 
+
+				step+=1
+				next
+			end
+
+			if (element==-1 and step==1)
+				
+				my_print="-x#{my_print}" 
+
+				step+=1
+				next
+			end
+
+			if element==1
+
+				my_print="+x^#{step}#{my_print}"
+				step+=1
+				next
+			end
+
+			if element==-1
+
+				my_print="-x^#{step}#{my_print}"
+				step+=1
+				next
+			end
+
+			if step==1
+
+				if element>0
+						my_print="+#{element}x#{my_print}"
+				else
+						my_print="#{element}x#{my_print}"
+				end
+
+				step+=1
+				next
+			end
+
+
+			if element>0
+				my_print="+#{element}x^#{step}#{my_print}"
+			else
+				my_print="#{element}x^#{step}#{my_print}"
+			end
+			
+			step+=1
+
+		end
+
+		my_print="0" if my_print==""
+		my_print=my_print[1..my_print.length-1] if my_print[0]=="+"
+
+		my_print
+
+
+	end
+
+
+	def my_mas
+		@my_mas
+
+	end
+
+end
+
+#my=Polynomial.new(ARGV)
+#puts my.create_polynomial
---
+++ polynomial/polynomial_test.rb
@@ -0,0 +1,64 @@
+require "rspec"
+require_relative "polynomial.rb"
+
+describe Polynomial do 
+
+  it "test_first_negative" do
+    p=Polynomial.new([-5,-2,1,0,8])
+    p.create_polynomial.should=="-5x^4-2x^3+x^2+8"
+  end
+
+  it "test_simple" do
+    p=Polynomial.new([1,0,7])
+    p.create_polynomial.should=="x^2+7"
+  end
+
+  it "test_first_minus_one" do
+    p=Polynomial.new([-1,-2,3,0])
+    p.create_polynomial.should=="-x^3-2x^2+3x"
+  end
+
+  it "test_all_zero" do
+    p=Polynomial.new([0,0,0])
+    p.create_polynomial.should=="0"
+  end
+
+  
+end
+ 
+=begin
+class PolynomialTest 
+  def setup
+    @p1 = Polynomial.new([-5,-2,1,0,8])
+    @p2 = Polynomial.new([1,0,7])
+    @p3 = Polynomial.new([-1,-2,3,0])
+    @p4 = Polynomial.new([0,0,0])
+  end
+ 
+  def test_first_negative
+    assert_equal("-5x^4-2x^3+x^2+8", @p1.to_s)
+  end
+ 
+  def test_simple
+    assert_equal("x^2+7", @p2.to_s)
+  end
+ 
+  def test_first_minus_one
+    assert_equal("-x^3-2x^2+3x", @p3.to_s)
+  end
+ 
+  def test_all_zero
+    assert_equal("0", @p4.to_s)
+  end
+ 
+  def test_error
+    e = assert_raise(ArgumentError) { Polynomial.new([1]) }
+    assert_match(/Two or more coefficients are required/, e.message)
+  end
+end
+ 
+m=PolynomialTest.new
+m.setup
+m.test_first_negative
+#Test::Unit::UI::Console::TestRunner.run(PolynomialTest)
+=end
